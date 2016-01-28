c603cbb3774f0fab85bdf04be9cee7d8800df38c
Acruid <shatter66@gmail.com>
2016-01-28 06:54:37 -0800

    Split nlscript encrypt/decrypt into lib-novalogic.

---
+++ Tools/lib-novalogic/Script/ScriptFile.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO;
+
+namespace Novalogic.Script
+{
+    public static class ScriptFile
+    {
+        private const uint SCR_HEADER = 0x01524353;
+        private const int SCR_HEADER_SZ = 0x04;
+
+        public static byte[] Encrypt(byte[] bytes, uint key)
+        {
+            var newBlock = new byte[bytes.Length];
+            Array.Copy(bytes, newBlock, bytes.Length);
+
+            XorBlock(newBlock, key, newBlock.Length);
+            SwapByteOrder(newBlock);
+
+            var body = new byte[bytes.Length + SCR_HEADER_SZ];
+            Array.Copy(newBlock, 0, body, 4, newBlock.Length);
+
+            var headerBytes = BitConverter.GetBytes(SCR_HEADER);
+            Array.Copy(headerBytes, 0, body, 0, headerBytes.Length);
+
+            return body;
+        }
+
+        public static byte[] Decrypt(byte[] bytes, uint key)
+        {
+            if (bytes.Length < SCR_HEADER_SZ)
+                throw new IOException("Set to decode, but input header is corrupt.");
+
+            var header = BitConverter.ToUInt32(bytes, 0);
+
+            if (header != SCR_HEADER)
+                throw new Exception("Set to decode, input header is wrong.");
+
+            var body = new byte[bytes.Length - SCR_HEADER_SZ];
+            Array.Copy(bytes, 4, body, 0, body.Length);
+
+            SwapByteOrder(body);
+            XorBlock(body, key, body.Length);
+            return body;
+        }
+
+        public static bool TryGetKey(string name, out uint key)
+        {
+            switch (name)
+            {
+                case "DF1":
+                    key = 0x04960552;
+                    break;
+                case "DF2":
+                    key = 0x01234567;
+                    break;
+                case "DF3":
+                    key = 0x01234567;
+                    break;
+                case "TFD":
+                    key = 0x01234567;
+                    break;
+                case "C4":
+                    key = 0x01234567;
+                    break;
+                case "BHD":
+                    key = 0x2A5A8EAD;
+                    break;
+                case "TS":
+                    key = 0x2A5A8EAD;
+                    break;
+                case "JO":
+                    key = 0x2A5A8EAD;
+                    break;
+                case "JO:B":
+                    key = 0xABEEFACE;
+                    break;
+                default:
+                    key = 0;
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static void XorBlock(byte[] input, uint key, int length)
+        {
+            var i = 0;
+
+            if (length <= 0)
+                return;
+
+            do
+            {
+                var temp1 = RotateLeft(key, 11);
+                var temp2 = RotateLeft(key + temp1, 4);
+
+                key = temp2 ^ 1;
+
+                input[i] ^= (byte) (temp2 ^ 1);
+
+                i++;
+            } while (i < length);
+        }
+
+        private static void SwapByteOrder(byte[] input)
+        {
+            var len = input.Length;
+            var half = len/2;
+
+            if (half <= 0)
+                return;
+
+            var front = 0;
+            var back = len - 1;
+            var i = half;
+            do
+            {
+                var temp = input[front];
+                input[front] = input[back];
+                input[back] = temp;
+
+                front++;
+                back--;
+                i--;
+            } while (i > 0);
+        }
+
+        private static uint RotateLeft(uint value, int count)
+        {
+            return (value << count) | (value >> (32 - count));
+        }
+    }
+}
--- Tools/scr/Program.cs
+++ Tools/scr/Program.cs
@@ -1,31 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
+using Novalogic.Script;
 
 namespace SCR
 {
     internal static class Program
     {
-        private const uint SCR_HEADER = 0x01524353;
-        private const int SCR_HEADER_SZ = 0x04;
-
-        private static Dictionary<string, uint> _keys;
-
         private static void Main(string[] args)
         {
-            _keys = new Dictionary<string, uint>
-            {
-                {"DF1", 0x04960552},
-                {"DF2", 0x01234567},
-                {"DF3", 0x01234567},
-                {"TFD", 0x01234567},
-                {"C4", 0x01234567},
-                {"BHD", 0x2A5A8EAD},
-                {"JO", 0x2A5A8EAD},
-                {"TS", 0x2A5A8EAD},
-                {"JO:B", 0xABEEFACE}
-            };
-
             FileInfo inputFile = null;
             var mode = Mode.ERROR;
             uint key = 0x0;
@@ -49,13 +31,12 @@ namespace SCR
                         {
                             var keyStr = args[i++];
 
-                            if (_keys.ContainsKey(keyStr))
+                            if (!ScriptFile.TryGetKey(keyStr, out key))
                             {
-                                key = _keys[keyStr];
-                            }
-                            else if (keyStr.Length == 10)
-                            {
-                                key = Convert.ToUInt32(keyStr.Substring(2), 16);
+                                if (keyStr.Length == 10)
+                                {
+                                    key = Convert.ToUInt32(keyStr.Substring(2), 16);
+                                }
                             }
                         }
                         break;
@@ -76,101 +57,29 @@ namespace SCR
             if (key == 0x0)
                 throw new ArgumentException("No key was specified.");
 
-            ConvertFile(inputFile, mode, key);
-        }
-
-        private static void ConvertFile(FileInfo file, Mode mode, uint key)
-        {
-            using (var stream = file.OpenRead())
+            using (var stream = inputFile.OpenRead())
             {
                 using (var reader = new BinaryReader(stream))
                 {
+                    var body = reader.ReadBytes((int) stream.Length);
+
                     if (mode == Mode.DECODE)
                     {
-                        if (stream.Length < SCR_HEADER_SZ)
-                            throw new IOException("Set to decode, but input header is corrupt.");
+                        body = ScriptFile.Decrypt(body, key);
 
-                        var header = reader.ReadUInt32();
-                        if (header != SCR_HEADER)
-                            throw new Exception("Set to decode, input header is wrong.");
-
-                        var body = reader.ReadBytes((int) (stream.Length - stream.Position));
-
-                        SwapByteOrder(body);
-
-                        XorBlock(body, key, body.Length);
-
-                        File.WriteAllBytes(file.FullName + ".txt", body);
+                        File.WriteAllBytes(inputFile.FullName + ".txt", body);
                     }
                     else if (mode == Mode.ENCODE)
                     {
-                        var body = reader.ReadBytes((int) stream.Length);
+                        body = ScriptFile.Encrypt(body, key);
 
-                        XorBlock(body, key, body.Length);
+                        var newName = inputFile.FullName.Substring(0,
+                            inputFile.FullName.Length - inputFile.Extension.Length);
 
-                        SwapByteOrder(body);
-
-                        var newName = file.FullName.Substring(0, file.FullName.Length - file.Extension.Length);
-
-                        using (var output = File.OpenWrite(newName))
-                        {
-                            using (var writer = new BinaryWriter(output))
-                            {
-                                writer.Write(SCR_HEADER);
-                                writer.Write(body);
-                            }
-                        }
+                        File.WriteAllBytes(newName, body);
                     }
                 }
             }
-        }
-
-        private static void SwapByteOrder(byte[] input)
-        {
-            var len = input.Length;
-            var half = len/2;
-
-            if (half <= 0)
-                return;
-
-            var front = 0;
-            var back = len - 1;
-            var i = half;
-            do
-            {
-                var temp = input[front];
-                input[front] = input[back];
-                input[back] = temp;
-
-                front++;
-                back--;
-                i--;
-            } while (i > 0);
-        }
-
-        private static void XorBlock(byte[] input, uint key, int length)
-        {
-            var i = 0;
-
-            if (length <= 0)
-                return;
-
-            do
-            {
-                var temp1 = key.RotateLeft(11);
-                var temp2 = (key + temp1).RotateLeft(4);
-
-                key = temp2 ^ 1;
-
-                input[i] ^= (byte) (temp2 ^ 1);
-
-                i++;
-            } while (i < length);
-        }
-
-        private static uint RotateLeft(this uint value, int count)
-        {
-            return (value << count) | (value >> (32 - count));
         }
 
         private enum Mode
